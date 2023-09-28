94ac1a7c439f5e48e2c1be77d5b607be83b8b63b
caleb1279 <caleb.dcaor@colnexsi.com.co>
2023-09-28 09:58:13 -0500

    ajuste de pruebas unitarias

--- test/unit/specs/Home.spec.js
+++ test/unit/specs/Home.spec.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import Home from '@/components/Home'
 
 describe('Home.vue', () => {
-  it('should render correct contents', () => {
+  it('deberia renderizar correctamente', () => {
     const Constructor = Vue.extend(Home)
     const vm = new Constructor().$mount()
     expect(vm.$el.querySelector('.container-dock h1').textContent)
